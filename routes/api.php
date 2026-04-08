9e2f094f726e1d710c09473efe97a9f217daa46b
Brenda <brendasinyal@gmail.com>
2026-04-08 12:31:40 +1200

    change route for temporary

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PaymentController;
 use App\Http\Controllers\Api\WebhookController;
 
-Route::post('/v1/payments', [PaymentController::class, 'store'])
-    ->middleware('api.key');
+Route::post('/v1/payments', [PaymentController::class, 'store']);
+    //->middleware('api.key');
 
-Route::get('/v1/payments/{reference}', [PaymentController::class, 'show'])
-    ->middleware('api.key');
+Route::get('/v1/payments/{reference}', [PaymentController::class, 'show']);
+    //->middleware('api.key');
 
 Route::post('/v1/webhooks/payments', [WebhookController::class, 'handle']);
 
