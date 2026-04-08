de48ada542d87add8cf6d53f0d91613f77f85fa1
Brenda <brendasinyal@gmail.com>
2026-04-08 14:35:47 +1200

    update sign phh

--- sign.php
+++ sign.php
@@ -1,6 +1,6 @@
 <?php
 
-$body = '{"payment_reference":"PAY-002-CZIH0W","external_transaction_id":"DW-TEST-002","status":"paid","paid_at":"2026-04-08 10:30:00"}';
+$body = '{"payment_reference":"PAY-004-MKSLKD","external_transaction_id":"DW-TEST-003","status":"paid","paid_at":"2026-04-08 14:30:00"}';
 $secret = 'MY_SUPER_SECRET_KEY_123';
 
 echo hash_hmac('sha256', $body, $secret);
